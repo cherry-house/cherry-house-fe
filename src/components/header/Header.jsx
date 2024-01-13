782862748cadf4512e88d349043b087c8ab24cb6
Jungyoon Moon <halfmoonjy99@pusan.ac.kr>
2024-01-13 21:40:27 +0900

    feat: 레이아웃 ui

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import "./Header.scss";
+import { ReactComponent as HeaderLogo } from "../../images/header_logo.svg";
+import { ReactComponent as SearchIcon } from "../../images/search_icon.svg";
+
+const Header = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbarMain">
+        <HeaderLogo />
+      </Link>
+      <div className="navbarSide">
+        <div className="searchBar">
+          <SearchIcon style={{ height: "20px", width: "20px" }} />
+          <span className="search">검색</span>
+        </div>
+        <Link to="/join" style={{ textDecoration: "none" }}>
+          <div className="navbarUser">Join</div>
+        </Link>
+        <Link to="/login" style={{ textDecoration: "none" }}>
+          <div className="navbarUser">Login</div>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
